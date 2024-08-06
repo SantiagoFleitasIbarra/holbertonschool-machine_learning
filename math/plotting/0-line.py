30eb4f167f9471eb1a7cee842faf00b3c0106700
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-08-06 06:15:25 +0000

    add task and fix bug style of task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""0-line"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Define line"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
