4c49ad9881d5611af1758a315b8477a9c70825de
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-08-08 15:09:28 +0000

    fix style

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -19,11 +19,11 @@ def bars():
 
     plt.bar(names, apples, width=0.5, color='red', label='apples')
     plt.bar(names, bananas, width=0.5, color='yellow', label='bananas',
-    bottom=apples)
+            bottom=apples)
     plt.bar(names, oranges, width=0.5, color='#ff8000', label='oranges',
-    bottom=apples + bananas)
+            bottom=apples + bananas)
     plt.bar(names, peaches, width=0.5, color='#ffe5b4', label='peaches',
-    bottom=apples + bananas + oranges)
+            bottom=apples + bananas + oranges)
 
     plt.ylabel('Quantity of Fruit')
     plt.yticks(np.arange(0, 81, 10))
