193e8723a4aa23ead1245ff7b419006e97e61985
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-08-08 15:14:34 +0000

    fix style

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def bars():
     """Define bars function"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     apples = fruit[0]
