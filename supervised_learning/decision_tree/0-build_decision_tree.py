5e3be29921f52179c93a0d4e1976dac6ffbd697b
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-08-15 00:47:02 +0000

    fix bug style

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -7,7 +7,16 @@ import numpy as np
 
 class Node():
     """class Node"""
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(
+            self,
+            feature=None,
+            threshold=None,
+            left_child=None,
+            right_child=None,
+            is_root=False,
+            depth=0
+            ):
+        """Initializing attributes"""
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -18,6 +27,7 @@ class Node():
         self.depth = depth
 
     def max_depth_below(self):
+        """Method for finding maximum node depths"""
         if self.is_leaf:
             return self.depth
         else:
@@ -26,20 +36,32 @@ class Node():
                 right_depth = self.right_child.max_depth_below()
                 return max(left_depth, right_depth)
 
+
 class Leaf(Node):
     """class leaf"""
     def __init__(self, value, depth=None):
+        """Initializing attributes"""
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        """Maximum node depth"""
         return self.depth
+
 
 class Decision_Tree():
     """class decision tree"""
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(
+            self,
+            max_depth=10,
+            min_pop=1,
+            seed=0,
+            split_criterion="random",
+            root=None
+            ):
+        """Initializing attributes"""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -53,4 +75,5 @@ class Decision_Tree():
         self.predict = None
 
     def depth(self):
+        """Depth"""
         return self.root.max_depth_below()
