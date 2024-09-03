4e9c08ddc9c4798c3350f8368e630af7606646ae
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-09-03 17:55:14 +0000

    add task and create readme

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0-create_placeholders"""
+
+import tensorflow.compat.v1 as tf
+
+def create_placeholders(nx, classes):
+    """returns two placeholders, x and y"""
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
