ac5e714dff044e24f7608877f2ccd33690f72366
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-09-04 19:46:03 +0000

    add task

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """6-train"""
 
+import tensorflow.compat.v1 as tf
+
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
